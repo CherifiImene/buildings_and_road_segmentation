064ba8b4eddd3cb4ce81a3d0260010e94e5d7597
Cherifi Imane <50464518+CherifiImene@users.noreply.github.com>
2023-02-01 21:33:41 +0100

    Added function to plot the loss and metrics evolution during the training

--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,22 @@
-from pytorch_lightning.callbacks import Callback
-import torch
-from data_handler.data import labelColors
-from data_handler.data_visualization_nd_stats import colorize12_mask
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
+def concat_dataframes(df_paths):
+    dfs = []
+    for path in df_paths:
+        dfs.append(pd.read_csv(path))
+    
+    merged_df = pd.concat(dfs)
+    return merged_df
+        
 def visualize_training_plots(metrics_file):
     
-    df = pd.read_csv(metrics_file)
+    if isinstance(metrics_file, list):
+        df = concat_dataframes(metrics_file)
+    else:
+        df = pd.read_csv(metrics_file)
+    
     train_metrics = df.iloc[::2]
     val_metrics = df.iloc[1::2]
     
@@ -23,38 +30,26 @@ def visualize_training_plots(metrics_file):
     val_mean_iou = val_metrics.val_mean_iou.values
     val_mean_acc = val_metrics.val_mean_accuracy.values
     
-    fig, axes = plt.subplot(1,3,figsize=(10,6))
+    fig, axes = plt.subplots(1,3,figsize=(15,6))
     axes[0].plot(epochs,train_loss,'r', label='Training loss')
     axes[0].plot(epochs,val_loss,'b', label='Validation loss')
     axes[0].set_title("Training and validation loss")
-    axes[0].xlabel('Epoch')
-    axes[0].ylabel('Loss Value')
+    axes[0].set_xlabel('Epoch')
+    axes[0].set_ylabel('Loss Value')
 
 
     axes[1].plot(epochs,train_mean_iou,'r', label='Training mean iou')
     axes[1].plot(epochs,val_mean_iou,'b', label='Validation mean iou')
     axes[1].set_title("Training and validation mean IOU")
-    axes[1].xlabel('Epoch')
-    axes[1].ylabel('Mean IOU Value')
-    axes[1].ylim([0, 1])
+    axes[1].set_xlabel('Epoch')
+    axes[1].set_ylabel('Mean IOU Value')
+    axes[1].set_ylim([0, 1])
     
     axes[2].plot(epochs,train_mean_acc,'r', label='Training mean accuracy' )
     axes[2].plot(epochs,val_mean_acc, 'b', label='Validation mean accuracy')
     axes[2].set_title("Training and validation mean Accuracy")
-    axes[2].xlabel('Epoch')
-    axes[2].ylabel('Mean Accuracy Value')
-    axes[2].ylim([0, 1])
+    axes[2].set_xlabel('Epoch')
+    axes[2].set_ylabel('Mean Accuracy Value')
+    axes[2].set_ylim([0, 1])
     plt.legend()
     plt.show()
-
-def visualize_sample_predictions(images,masks,model):
-    
-    pass
-        
-        
-        
-        
-        
-        
-        
-        
